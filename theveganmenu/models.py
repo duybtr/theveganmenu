96ce42d6da8e644e9305350e79be37f7ec99b497
Duy Tran <duytran2310@gmail.com>
2025-06-29 10:01:11 -0500

    modified layout

--- theveganmenu/models.py
+++ theveganmenu/models.py
@@ -34,7 +34,8 @@ class Restaurant(models.Model):
     )
     is_fully_vegan = models.BooleanField(default=False)
     address = models.CharField(max_length=300)
-    link = models.CharField(max_length=500)
+    website_link = models.CharField(max_length=500)
+    map_link = models.CharField(max_length=500)
     longitude = models.DecimalField(max_digits=20, decimal_places=13, default=0)
     latitude = models.DecimalField(max_digits=20, decimal_places=13, default=0)
 
